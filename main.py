e1aabe9e339630197bee7a5356a86c7f95aa3a89
eeoos <119295425+eeoos@users.noreply.github.com>
2023-09-21 09:06:41 +0900

    Add Flask
    blank attribute error

--- main.py
+++ main.py
@@ -1,43 +1,47 @@
+from flask import Flask, render_template, request, redirect, send_file
 from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
 from extractors.incruit import extract_incruit_jobs
 from extractors.saramin import extract_saramin_jobs
-#from extractors.wanted import extract_wanted_jobs
-
 from file import save_to_file
 
-
-keyword = input("What do you want to search for?")
-
-indeed = extract_indeed_jobs(keyword)
-wwr = extract_wwr_jobs(keyword)
-incruit = extract_incruit_jobs(keyword)
-saramin = extract_saramin_jobs(keyword)
-
-jobs = indeed + wwr + incruit + saramin
-
-save_to_file(keyword, jobs)
+app = Flask("JobScrapper")
 
 
 
-"""
-wanted = extract_wanted_jobs(keyword)
-wanted 왜 jobs이 8개밖에 안뜨는지 모르겠음
+# Flask는 user가 이 주소의 page를 방문했을 때 이 함수를 호출해야 하는 것을 알게 됨
+@app.route("/") # @가 있는 코드를 함수 위에 위치시켜야 함 (decorator), syntatic sugar
+def home():
+   return render_template("home.html", name="nico")
 
+db = {}
 
+@app.route("/search")
+def search():
+    keyword = request.args.get("keyword")
+    if keyword == None or keyword == "":
+        return redirect("/")
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        indeed = extract_indeed_jobs(keyword)
+        wwr = extract_wwr_jobs(keyword)
+   # incruit = extract_incruit_jobs(keyword)
+   # saramin = extract_saramin_jobs(keyword)
 
-challengevb
-part 1. 다른 구직 사이트로 가서 자신만의 추출기 만들어보기
-part 1. incruit (requests 사용) 완료 - 매우 미흡
-태그를 불러올 때 클래스명을 활용하지 않고 태그 순서로 불러와 예외인 경우가 발생하여 오류 발생
-밑의 링크를 통해 클래스명을 활용하여 추출하는 방법 터득. + get_text()
-https://replit.com/@rorodeuni/Scrapper#extractors/MyChallenge.py 참고해서 공부하기
-part 1. saramin (selenium 사용) 완료 
-part 1. wanted (selenium 사용) 무한 스크롤 적용하기: jobs가 8개밖에 안뜸 모르겠음
+        jobs = indeed + wwr
+        db[keyword] = jobs
+   # + incruit + saramin
+    return render_template("search.html", keyword=keyword, jobs=jobs)
 
-part 2. indeed에서 10개의 페이지를 스크랩 할 수 있는지 확인하고 시도
-part 2. indeed 맨 끝까지 스크랩 할 수 있는 코드 구현
-마지막 페이지에서 더 갈 수 있다면 오류 발생
-Flask는 Python을 이용해서 웹사이트를 구축할 수 있는 초소형 micro framework
-"""
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+    save_to_file(keyword, db[keyword])    
+    return send_file(f"{keyword}.csv", as_attachment=True)
+app.run("127.0.0.1") # Replit에게 만든 웹사이트로의 접속을 열어달라고 알려주는 코드
 
